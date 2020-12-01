594c6b8553e109648c72694315d697432929434c
Magno Nascimento <magno@magnonascimento.com>
2020-12-01 11:49:14 -0300

    Refactoring on pages structure

---
+++ src/pages/professional/index.tsx
@@ -0,0 +1,11 @@
+import { FC, HTMLAttributes } from 'react';
+import JobInfo from '../../components/molecules/JobInfo';
+import MasterPage from '../../components/templates/Masterpage';
+
+const Professional: FC<HTMLAttributes<HTMLDivElement>> = () => (
+    <MasterPage>
+        <JobInfo companyName="Avanade" jobTitle="Desenvolvedor Fullstack" jobPeriod="Outubro 2019 - Dezembro 2019" />
+    </MasterPage>
+);
+
+export default Professional;

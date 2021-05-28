5e5bd6d7e1cdbb8ca3b472ce90cb222b1834828e
Magno Nascimento <magno@magnonascimento.com>
2021-05-28 17:10:57 -0300

    Made first presentation as non-ssr content

---
+++ src/components/molecules/Greeting.tsx
@@ -0,0 +1,48 @@
+import { FC, HTMLAttributes, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { about } from '../i18n/pt.json';
+import { Paragraph } from '../atoms/Text';
+
+const GOOD_MORNING_HOUR = 6;
+const GOOD_AFTERNOON_HOUR = 12;
+const GOOD_EVENING_HOUR = 18;
+const GOOD_NIGHT_HOUR = 20;
+
+const AboutParagraph = styled(Paragraph)`
+    margin: 0.5rem 0;
+    line-height: 1.75rem;
+
+    text-align: justify;
+    text-justify: inter-word;
+`;
+
+const Greeting: FC<HTMLAttributes<HTMLDivElement>> = () => {
+    const { greetings } = about;
+    const [greeting, setGreeting] = useState(greetings.default);
+
+    useEffect(() => {
+        const currentDate = new Date().getHours();
+
+        switch (true) {
+            case currentDate > GOOD_NIGHT_HOUR || currentDate < GOOD_MORNING_HOUR:
+                setGreeting(greetings.goodNight);
+                break;
+            case currentDate > GOOD_EVENING_HOUR:
+                setGreeting(greetings.goodEvening);
+                break;
+            case currentDate > GOOD_AFTERNOON_HOUR:
+                setGreeting(greetings.goodAfternoon);
+                break;
+            case currentDate > GOOD_MORNING_HOUR:
+                setGreeting(greetings.goodMorning);
+                break;
+            default:
+                setGreeting(greetings.default);
+                break;
+        }
+    }, [window]);
+
+    return <AboutParagraph>{greeting}</AboutParagraph>;
+};
+
+export default Greeting;
--- src/components/organisms/About/About.tsx
+++ src/components/organisms/About/About.tsx
@@ -1,4 +1,5 @@
-import { FC, HTMLAttributes, useEffect, useState } from 'react';
+import { FC, HTMLAttributes } from 'react';
+import dynamic from 'next/dynamic';
 import styled from 'styled-components';
 import { sprintf } from 'sprintf-js';
 import moment from 'moment';
@@ -6,10 +7,7 @@ import { about } from '../../i18n/pt.json';
 import { Paragraph } from '../../atoms/Text';
 import SocialMenu from '../../molecules/SocialMenu';
 
-const GOOD_MORNING_HOUR = 6;
-const GOOD_AFTERNOON_HOUR = 12;
-const GOOD_EVENING_HOUR = 18;
-const GOOD_NIGHT_HOUR = 20;
+const DynamicGreeting = dynamic(() => import('../../molecules/Greeting'), { ssr: false });
 
 const BIRTH_DATE = moment('1997-09-05', 'YYYY-MM-DD');
 const currentAge = Math.ceil(moment.duration(moment().diff(BIRTH_DATE)).years());
@@ -38,34 +36,11 @@ const AboutParagraph = styled(Paragraph)`
 `;
 
 const Professional: FC<HTMLAttributes<HTMLDivElement>> = () => {
-    const { greetings, presentation, description } = about;
-    const [greeting, setGreeting] = useState(greetings.default);
-
-    useEffect(() => {
-        const currentDate = new Date().getHours();
-
-        switch (true) {
-            case currentDate > GOOD_NIGHT_HOUR || currentDate < GOOD_MORNING_HOUR:
-                setGreeting(greetings.goodNight);
-                break;
-            case currentDate > GOOD_EVENING_HOUR:
-                setGreeting(greetings.goodEvening);
-                break;
-            case currentDate > GOOD_AFTERNOON_HOUR:
-                setGreeting(greetings.goodAfternoon);
-                break;
-            case currentDate > GOOD_MORNING_HOUR:
-                setGreeting(greetings.goodMorning);
-                break;
-            default:
-                setGreeting(greetings.default);
-                break;
-        }
-    }, []);
+    const { presentation, description } = about;
 
     return (
         <AboutContainer>
-            <AboutParagraph>{greeting}</AboutParagraph>
+            <DynamicGreeting />
             <AboutParagraph>{sprintf(presentation, currentAge)}</AboutParagraph>
             {description.map((paragraph, index) => (
                 <AboutParagraph key={`about-${index}`}>{paragraph}</AboutParagraph>
